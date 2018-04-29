f0a60d7163bfca08fd9205ac741a623ef52d4b5b
katherinex <kathexio@gmail.com>
2018-04-28 20:07:39 -0500

    Interfaces were created.

---
+++ app/src/main/java/com/katherine/pruebarappi2/activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.katherine.pruebarappi2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.katherine.pruebarappi2.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
